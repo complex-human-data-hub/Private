ad67065d945b7ea04bdbee7fcedae742985bf3f4
S-J-HAN <42526679+S-J-HAN@users.noreply.github.com>
2018-08-20 16:47:22 +1000

    Update parser.py
    names like 'expression_atom' and 'expression_molecules' are temporary placeholders. Current parser should be able to parse everything in the google doc.

--- parser.py
+++ parser.py
@@ -1,32 +1,72 @@
 from arpeggio import *
 from arpeggio import RegExMatch as _
 
-def user_entry(): return ZeroOrMore(statement), EOF
-def statement(): return identifier, ["=", "~"], [function, list, expression]
+# Entry
+def user_entry(): return ZeroOrMore([
+                                      (statement, Optional(comment)),
+                                      (declare_function, Optional(comment)),
+                                      comment
+                                    ]), EOF
+
+# Statement
+def statement(): return [
+                          assign_statement,
+                          import_statement,
+                        ]
+def assign_statement(): return any_identifier, ["=", "~"], [function, list, expression]
+def import_statement(): return Optional("from", identifier), "import", identifier, Optional("as", identifier)
+def return_statement(): return "return", [("(", expression, ")"), expression]
+def define_statement(): return "def", identifier, "(", identifier, ZeroOrMore(",", identifier), ")", ":"
+
+# Building blocks
 def atom(): return [identifier, literal]
 def literal(): return [number_literal, string_literal]
 def number_literal(): return Optional(["+", "-"]), _(r'(\d)+\.?(\d)+')
 def string_literal(): return [_(r'\".+\"'), _(r'\'.+\'')]
+
+# Identifier
 def identifier(): return _(r'(\w)+')
-def identifier_index(): return identifier, OneOrMore("[", expression, "]")
-def identifier_attribute(): return identifier, OneOrMore((".", identifier))
-def function(): return identifier, "(", Optional([list, expression], ZeroOrMore((",", [list, expression]))), ")"
-def list(): return "[", comprehension, "]"
+def index_identifier(): return identifier, OneOrMore("[", [index, expression], "]")
+def attribute_identifier(): return identifier, OneOrMore((".", identifier))
+def any_identifier(): return [index_identifier, attribute_identifier, identifier]
+
+# Function
+def function(): return (identifier, "(",
+                        Optional([assign_statement, list, expression],
+                        ZeroOrMore((",", [assign_statement, list, expression]))),
+                        ")")
+def declare_function(): return define_statement, ZeroOrMore(statement), return_statement
+
+# Expressions
+def expression_atom(): return [index_identifier, attribute_identifier, function, atom]
+def expression_molecule(): return [
+                                    ("(", expression_molecule, ")", ZeroOrMore(["*", "\\", "**"], expression_molecule)),
+                                    (expression_atom, ZeroOrMore(["*", "\\", "**"], expression_molecule))
+                                  ]
 def expression(): return [
-                           identifier_index, # eg. mu[subjects]
-                           identifier_attribute, # eg. latitude.hier.mu
-                           atom # eg. "IFTTTButton"
+                            ("(", expression, ")", ZeroOrMore(["+", "-"], expression)),
+                            (expression_molecule, ZeroOrMore(["+", "-"], expression))
                          ]
+
+# List
+def list(): return "[", [comprehension, array], "]"
 def comprehension(): return expression, comp_for
 def comp_for(): return "for", expression, "in", expression, ZeroOrMore([comp_for, comp_if])
 def comp_if(): return "if", boolean_expression
+def array(): return expression, ZeroOrMore(",", expression)
+
+# Boolean
 def boolean(): return expression, ["==", "!="], expression
 def boolean_expression(): return [
                                    (Optional("!"), "(", boolean_expression, ")", ZeroOrMore(["and", "or"], boolean_expression)),
                                    (boolean, ZeroOrMore(["and", "or"], boolean_expression))
                                  ]
 
-test = "HappyDays = unique([e.day for e in events if e.type == 'IFTTTButton'])"
+# Misc.
+def comment(): return _(r'\#.+')
+def index(): return Optional(number_literal), ":", Optional(number_literal), Optional(":", Optional(number_literal))
+
+test = "latitudes ~ normal(mu, sigma)"
 
 # Parse
 parser = ParserPython(user_entry)
