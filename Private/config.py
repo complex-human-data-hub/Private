6c8868ade09217c908bfaa23c52818fa81ee06e0
Malinga Perera <malingaonfire@gmail.com>
2019-04-02 14:32:55 +1100

    check ppserver.conf exist  #38

--- Private/config.py
+++ Private/config.py
@@ -21,10 +21,13 @@ s3_log_level = logging.CRITICAL
 
 ppservers = ()
 ppservers_list = []
-with open('ppserver.conf', 'r') as f:
-    for line in f.readlines():
-        s = line.strip()
-        if s:
-            ppservers_list.append(s)
+
+exists = os.path.isfile('ppserver.conf')
+if exists:
+    with open('ppserver.conf', 'r') as f:
+        for line in f.readlines():
+            s = line.strip()
+            if s:
+                ppservers_list.append(s)
 
 ppservers = tuple(ppservers_list)
