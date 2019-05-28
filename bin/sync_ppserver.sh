2bf2a0fbce152619e7adcd9e069dd691624b2a78
Ben Stone <benjamin.stone@gmail.com>
2019-05-28 16:36:24 +1000

    Fix worker update

--- bin/sync_ppserver.sh
+++ bin/sync_ppserver.sh
@@ -3,9 +3,10 @@ while read p;
 do
 	s=$(echo $p | cut -f1 -d":") # Remove the port
 	echo $s
-	rsync -avuzpor --exclude .git --exclude .gitignore . ubuntu@$s:/home/ubuntu/venv/lib/python2.7/site-packages/Private
+	#rsync -avuzpor --exclude .git --exclude .gitignore . ubuntu@$s:/home/ubuntu/venv/lib/python2.7/site-packages/Private
 	ssh -oStrictHostKeyChecking=no $s /bin/bash <<EOF
 cd Private
+git checkout master
 git pull
 sudo systemctl restart ppserver.service
 EOF
