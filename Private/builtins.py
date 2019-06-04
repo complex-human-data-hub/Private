0e78bc9f65ea655f4dd80456d98c28068e26dd69
Malinga Perera <malingaonfire@gmail.com>
2019-06-04 13:30:31 +1000

    changes to builtins based on the python builtins #57

--- Private/builtins.py
+++ Private/builtins.py
@@ -204,14 +204,14 @@ def private_std(x):
 def private_abs(x):
     return abs(x)
 
-def private_all(x):
-    return all(x)
+def private_all(iterable):
+    return all(iterable)
 
-def private_any(x):
-    return any(x)
+def private_any(iterable):
+    return any(iterable)
 
-def private_bin(x):
-    return bin(x)
+def private_bin(number):
+    return bin(number)
 
 def private_bool(x):
     return bool(x)
@@ -219,41 +219,41 @@ def private_bool(x):
 def private_chr(x):
     return chr(x)
 
-def private_cmp(x):
-    return cmp(x)
+def private_cmp(x, y):
+    return cmp(x, y)
 
-def private_complex(x):
-    return complex(x)
+def private_complex(real, *imag):
+    return complex(real, *imag)
 
 def private_dict(x):
     return dict(x)
 
-def private_divmod(x):
-    return divmod(x)
+def private_divmod(x, y):
+    return divmod(x, y)
 
-def private_enumerate(x):
-    return list(enumerate(x))
+def private_enumerate(iterable, start=0):
+    return list(enumerate(iterable, start))
 
 def private_exp(x):
     return numpy.exp(x)
 
-def private_filter(x):
-    return filter(x)
+def private_filter(function, iterable): # cannot use as we can't take functions in Private
+    return filter(function, iterable)
 
 def private_float(x):
     return float(x)
 
-def private_format(x):
-    return format(x)
+def private_format(value, format_spec=None):
+    return format(value, format_spec)
 
 def private_frozenset(x):
     return frozenset(x)
 
-def private_getattr(x):
-    return getattr(x)
+def private_getattr(object, name, default=None):
+    return getattr(object, name, default)
 
-def private_hasattr(x):
-    return hasattr(x)
+def private_hasattr(p_object, name):
+    return hasattr(p_object, name)
 
 def private_hex(x):
     return hex(x)
@@ -261,32 +261,32 @@ def private_hex(x):
 def private_int(x):
     return int(x)
 
-def private_isinstance(x):
-    return isinstance(x)
+def private_isinstance(obj, type):
+    return isinstance(obj, type)
 
-def private_issubclass(x):
-    return issubclass(x)
+def private_issubclass(x, y):
+    return issubclass(x, y)
 
-def private_iter(x):
-    return iter(x)
+def private_iter(source, sentinel=None):
+    return iter(source, sentinel)
 
 def private_len(x):
     return len(x)
 
 def private_list(x):
-    return tuple(x)
+    return list(x)
 
 def private_long(x):
     return long(x)
 
-def private_map(x):
-    return map(x)
+def private_map(function, iterable): # cannot use as we can't take functions in Private
+    return map(function, iterable)
 
-def private_min(x):
-    return min(x)
+def private_min(*args, **kwargs):
+    return min(*args, **kwargs)
 
-def private_max(x):
-    return max(x)
+def private_max(*args, **kwargs):
+    return max(*args, **kwargs)
 
 def private_object(x):
     return object(x)
@@ -294,11 +294,11 @@ def private_object(x):
 def private_oct(x):
     return oct(x)
 
-def private_ord(x):
-    return ord(x)
+def private_ord(ch):
+    return ord(ch)
 
-def private_pow(x):
-    return pow(x)
+def private_pow(x, y, z=None):
+    return pow(x, y, z)
 
 def private_open(name, per): ####### WARNING Do not ship with this line in ###########
     return open(name, per)
@@ -306,11 +306,11 @@ def private_open(name, per): ####### WARNING Do not ship with this line in #####
 def private_property(x):
     return property(x)
 
-def private_range(x):
-    return range(x)
+def private_range(*args):
+    return range(*args)
 
-def private_reduce(x):
-    return reduce(x)
+def private_reduce(function, iterable, initializer=None):  # cannot use as we can't take functions in Private
+    return reduce(function, iterable, initializer=initializer)
 
 def private_repr(x):
     return repr(x)
@@ -318,17 +318,17 @@ def private_repr(x):
 def private_reversed(x):
     return reversed(x)
 
-def private_round(x):
-    return round(x)
+def private_round(number, ndigits=None):
+    return round(number, ndigits=ndigits)
 
-def private_set(x):
-    return frozenset(x)
+def private_set(iterable):
+    return frozenset(iterable)
 
-def private_slice(x):
-    return slice(x)
+def private_slice(*args):
+    return slice(*args)
 
-def private_sorted(x):
-    return sorted(x)
+def private_sorted(iterable, cmp=None, key=None, reverse=False):
+    return sorted(iterable, cmp=cmp, key=key, reverse=reverse)
 
 def private_sqrt(x):
     return math.sqrt(x)
@@ -336,8 +336,8 @@ def private_sqrt(x):
 def private_str(x):
     return str(x)
 
-def private_sum(x):
-    return sum(x)
+def private_sum(iterable, start=0):
+    return sum(iterable, start)
 
 def private_tuple(x):
     return tuple(x)
@@ -348,17 +348,17 @@ def private_type(x):
 def private_unichr(x):
     return unichr(x)
 
-def private_unicode(x):
-    return unicode(x)
+def private_unicode(obj):
+    return unicode(obj)
 
-def private_vars(x):
-    return vars(x)
+def private_vars(p_object=None):
+    return vars(p_object)
 
-def private_xrange(x):
-    return xrange(x)
+def private_xrange(start, stop, *step):
+    return xrange(start, stop, *step)
 
-def private_zip(x):
-    return zip(x)
+def private_zip(*iterables):
+    return zip(*iterables)
 
 def private_fft(x, seg_size=-1):
     return pre.fft(x, seg_size)
