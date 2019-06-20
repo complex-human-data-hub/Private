8fa343e79a7117f9797e6f14a8c8e7154982f6e7
Ben Stone <benjamin.stone@gmail.com>
2019-06-20 05:22:57 +0000

    Add IP into logging

--- terminal-server/show-users.py
+++ terminal-server/show-users.py
@@ -6,13 +6,13 @@ now = time.time()
 
 def format_output(item):
     inactive_mins = int((now - item.get('access_time')) / 60.)
-    return "{}\t{}\t{}".format(inactive_mins, item.get('port'), item.get('uid'))
+    return "{}\t{}\t{}\t{}\t{}".format(inactive_mins, item.get('port'), item.get('uid'), item.get('ip', ''), item.get('ua', ''))
 
 
 server_shelf = shelvelock.open(config.privateserver_shelf)
 
 
-print "Inactive\tPort\tUID"
+print "Inactive\tPort\tUID\tIP\tUA"
 for key in server_shelf.keys():
     print format_output( json.loads(server_shelf.get(key)) )
 
