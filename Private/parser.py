a33e2cc302b73747b76c90f4b2b30830f4f0229c
Malinga Perera <malingaonfire@gmail.com>
2019-10-08 18:27:55 +1100

    changes to the parser allow indexing of expressions and functions #119

--- Private/parser.py
+++ Private/parser.py
@@ -91,7 +91,7 @@ def list_comprehension():       return "[", expression, "for", identifier_list, 
 def enumerated_list():          return "[", ZeroOrMore(expression, ","), expression, "]"
 def private_list():             return [list_comprehension, enumerated_list]
 def bracketed_expression():     return "(", expression, ")"
-def factor():                   return [function_call, method_call, indexed_variable, bracketed_expression, (notsym, factor), private_list, atom], Optional(leftsquarebrack, expression, colon, expression, rightsquarebrack)
+def factor():                   return [function_call, method_call, indexed_variable, bracketed_expression, (notsym, factor), private_list, atom], Optional(leftsquarebrack, expression, ZeroOrMore(colon, expression), rightsquarebrack)
 def term():                     return factor, ZeroOrMore(["*","/"], factor)
 def simple_expression():        return Optional(["+", "-"]), term, ZeroOrMore(["+", "-"], term)
 def comparison():               return simple_expression, ZeroOrMore(relation, simple_expression)
