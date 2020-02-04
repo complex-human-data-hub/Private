e4b037052c7c4b134d5633b1f92ee5cfa1477302
Malinga Perera <malingaonfire@gmail.com>
2020-02-04 11:43:12 +1100

    (py3) Changes to setup and requirement files #67

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=['Private'],
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
