d67d07f51fbd8624d867d6f93ecfb2f1188bb019
Malinga Perera <malingaonfire@gmail.com>
2020-02-04 11:18:30 +1100

    (py3) Changes to 2 unused files #67

--- Private/fakebuttons.py
+++ Private/fakebuttons.py
@@ -1,9 +1,11 @@
-from fakedata import FakeEvent
+from __future__ import print_function
+from __future__ import absolute_import
+from .fakedata import FakeEvent
 
-from demo_events import data
+from .demo_events import data
 
 users = set([e["UserId"] for e in data])
 
-print users
+print(users)
 
 
