627a47e0ed7e2f5ee429aea9fb331618e2b36b91
Malinga Perera <malingaonfire@gmail.com>
2020-05-03 13:51:59 +1000

    Fixing the order issue #188

--- Private/graph.py
+++ Private/graph.py
@@ -923,6 +923,8 @@ class graph:
             if name not in self.uptodate[user]:
                 result = result and self.checkdown(user, name)
                 result = result and self.checkup(user, name)
+            if name in self.deterministic & self.probabilistic:
+                result = result and self.checkdown(user, name)
         return result
 
     @staticmethod
