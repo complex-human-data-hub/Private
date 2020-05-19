04fd4abc1a1d0b6556fb3092994e0ab597cbca2b
Ubuntu <ubuntu@ip-172-31-6-104.us-west-2.compute.internal>
2020-05-19 00:23:51 +0000

    Fix create workers script

--- bin/create-workers.sh
+++ bin/create-workers.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-port=60000
-workers=3
+port=60004
+workers=22
 endworker=$(($port+$workers))
 for i in $(seq $port 1 $endworker)
 do
 
 daskservice=$(<dask-worker.service)
 newservice=dask-worker-$i.service
-outfile=/tmp/$newservice
+outfile=/etc/systemd/system/$newservice
+tmpfile=/tmp/$newservice
+
 echo $outfile
-echo "${daskservice/WORKER_PORT/$i}" > $outfile
-echo "sudo systemctl enable $newservice"
-echo "sudo systemctl restart $newservice"
+echo "${daskservice/WORKER_PORT/$i}" > $tmpfile
+sudo mv $tmpfile $outfile
+sudo systemctl enable $newservice
+sudo systemctl restart $newservice
 done; 
