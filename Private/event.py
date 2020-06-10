ba4b0072546f7c285407d796c94e9d51b3226a3e
Malinga Perera <malingaonfire@gmail.com>
2020-06-10 17:01:35 +1000

    removing aws_profile #196

--- Private/event.py
+++ Private/event.py
@@ -8,8 +8,7 @@ class Event:
     aws_profile = None
     def __init__(self, mydict, aws_profile=None):
         self.__dict__ = mydict
-        self.convert_data_files()
-        self.aws_profile = aws_profile
+        self.convert_data_files(aws_profile)
 
     def __repr__(self):
         return pp.pformat(self.__dict__)
@@ -20,10 +19,10 @@ class Event:
     def __str__(self):
         return pp.pformat(self.__dict__)
 
-    def convert_data_files(self):
+    def convert_data_files(self, aws_profile):
         for key in list(self.__dict__):
             if key.endswith("DataFiles") and not key.startswith('has'):
-                self.__dict__[key + "Itr"] = FileIterator(self.__dict__[key], aws_profile=self.aws_profile)
+                self.__dict__[key + "Itr"] = FileIterator(self.__dict__[key], aws_profile=aws_profile)
                 del self.__dict__[key]
 
     def __getitem__(self, item):
