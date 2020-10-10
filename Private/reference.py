cf6e15ae453a5df81e5d2eb1c51bb449cc68b277
Malinga Perera <malingaonfire@gmail.com>
2020-10-10 16:08:06 +1100

    new reference changes #266

--- Private/reference.py
+++ Private/reference.py
@@ -13,18 +13,14 @@ class Reference:
     def __init__(self, key, value, keep_existing=False):
         self.key = key
         if not keep_existing:
-            print("saving to disk")
             Private.disk_helper.save_results(key, value)
         self.display_value = self.get_display_value(value)
 
     def value(self):
         if Private.redis_helper.if_exist(self.key):
-            print("getting from redis")
             return Private.redis_helper.read_results(self.key)
         else:
-            print("getting from disk")
             value = Private.disk_helper.read_results(self.key)
-            print("save to redis")
             Private.redis_helper.save_results(self.key, value)
             return value
 
